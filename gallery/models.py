e26be156a86a1ff9593ece7fb0cf6302d1c1d0c6
“Edu58” <edumuriithi58@gmail.com>
2022-05-28 13:26:37 +0300

    adds Images db model

--- gallery/models.py
+++ gallery/models.py
@@ -8,3 +8,11 @@ class Categories(models.Model):
 
 class Locations(models.Model):
     name = models.CharField(max_length=50)
+
+
+class Images(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(null=False, blank=False, upload_to='images/')
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    location = models.ForeignKey(Locations, on_delete=models.CASCADE)
