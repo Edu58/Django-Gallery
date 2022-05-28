f3adf902bf1a1405ceca9ee36cd81efaf792672a
“Edu58” <edumuriithi58@gmail.com>
2022-05-28 16:51:21 +0300

    feat: show all images at home

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Categories, Locations, Images
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    all_images = Images.objects.all()
+    return render(request, 'index.html', {'all_images': all_images})
