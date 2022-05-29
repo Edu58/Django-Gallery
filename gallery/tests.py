eb1c9151be3ef9c11f823047dc320d88ea1e893f
“Edu58” <edumuriithi58@gmail.com>
2022-05-29 10:43:22 +0300

    adds tests for Categories db model

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Categories, Locations, Images
+
 
 # Create your tests here.
+class TestCategories(TestCase):
+    def setUp(self) -> None:
+        self.example_category = Categories(name="New Category")
+
+    def test_category_instance(self):
+        self.assertTrue(isinstance(self.example_category, Categories))
